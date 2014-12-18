24d77502f3b1db74ba2b9c5201ec7e665d64f495
Lars Hesel Christensen <lars@larshesel.dk>
2014-12-18 11:24:00 +0100

    Add functionality to parse the entire blockchain file

--- lib/parse_blockchain.ex
+++ lib/parse_blockchain.ex
@@ -1,9 +1,26 @@
 defmodule ParseBlockChain do
   @blockchain "/home/nobackup/blockhain/bootstrap.dat"
-  @readsize 8192
+  @readsize 1000000
 
   def openblockchain do
     File.open(@blockchain, [:read])
+  end
+
+  def parse_entire_file do
+    {:ok, file} = openblockchain()
+    count_all_blocks(file, <<>>, 0)
+    File.close(file)
+  end
+
+  def count_all_blocks(file, rest, num_blocks) do
+    case parse_next_chunk(file, rest) do
+      [] ->
+        :done
+      {:ok, blocks, rest} ->
+        num_blocks = num_blocks + length(blocks)
+        IO.puts("Parsed #{num_blocks} blocks...")
+        count_all_blocks(file, rest, num_blocks)
+    end
   end
 
   def parse_next_chunk(file, rest) do
@@ -28,10 +45,10 @@ defmodule ParseBlockChain do
     case res do
       {:ok, block, rest} ->
         {:ok, parsed_block, <<>>} = Parser.parse_block(block)
-        IO.puts("parsed block")
+        #IO.puts("parsed block")
         parse_blocks(rest, [parsed_block | blocks])
       {:error, :incomplete_block, rest} ->
-        IO.puts("incomplete block")
+        #IO.puts("incomplete block")
         {:ok, Enum.reverse(blocks), rest}
     end
   end
